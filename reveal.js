b498968ec60337e02c3a80eec3818f4919e9272a
ryan de melo morais <ryanmelo2325@gmail.com>
2024-09-04 17:56:55 -0300

    att normal

---
+++ reveal.js
@@ -0,0 +1,127 @@
+
+window.revelar = ScrollReveal ({reset:true})
+
+//TOPO DO SITE
+revelar.reveal('.efeito-txt-topo',
+{
+    duration: 2000,
+    distance: '90px'
+})
+
+revelar.reveal('.efeito-img-topo',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+//TITULOS DAS SESSÕES
+revelar.reveal('.titulo',
+{
+    duration: 2000,
+    distance: '90px'
+})
+
+//SESSÃO DE ESPECIALIDADES
+revelar.reveal('.efeito-especialidades1',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+revelar.reveal('.efeito-especialidades2',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1000
+})
+
+revelar.reveal('.efeito-especialidades3',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1500
+})
+
+//SESSÃO SOBRE
+revelar.reveal('.efeito-img-sobre',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 500,
+    origin:'left'
+})
+
+revelar.reveal('.efeito-txt-sobre',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 500,
+    origin:'left'
+})
+
+//SESSÃO PORTIFÓLIO
+revelar.reveal('.efeito-img-port1',
+{
+    duration: 1500,
+    distance: '90px',
+    delay: 1000,
+    origin:'right'
+})
+revelar.reveal('.efeito-img-port2',
+{
+    duration: 1500,
+    distance: '90px',
+    delay: 1500,
+    origin:'right'
+})
+revelar.reveal('.efeito-img-port3',
+{
+    duration: 1500,
+    distance: '90px',
+    delay: 2000,
+    origin:'right'
+})
+
+//SESSÃO CONTATO
+
+revelar.reveal('.efeito-txt-cntt1',
+{
+    duration: 1000,
+    distance: '90px',
+    delay: 1000,
+    origin:'bottom'
+})
+
+revelar.reveal('.efeito-txt-cntt2',
+{
+    duration: 1000,
+    distance: '90px',
+    delay: 1200,
+    origin:'bottom'
+})
+
+revelar.reveal('.efeito-txt-cntt3',
+{
+    duration: 1000,
+    distance: '90px',
+    delay: 1400,
+    origin:'bottom'
+})
+
+revelar.reveal('.efeito-txt-cntt4',
+{
+    duration: 1000,
+    distance: '90px',
+    delay: 1600,
+    origin:'bottom'
+})
+
+revelar.reveal('.btn-enviar',
+{
+    duration: 1000,
+    distance: '90px',
+    delay: 1800,
+    origin:'bottom'
+})
